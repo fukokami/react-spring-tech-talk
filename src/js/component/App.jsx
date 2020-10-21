c3628f27443ddebf4a8585c955a2889724470656
Doan Thanh Thang <thangdt@newit.co.jp>
2020-10-21 10:01:26 +0700

    🎉 setting porject

---
+++ src/js/component/App.jsx
@@ -0,0 +1,14 @@
+'use strict';
+
+
+import React from 'react';
+
+export default function App() {
+
+    return (
+        <div>
+            React spring teach talk
+        </div>
+    );
+    
+}
