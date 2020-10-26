b47fbfb8f3c91a092df90b93d5d1f83219033c35
Doan Thanh Thang <thangdt@newit.co.jp>
2020-10-26 17:02:47 +0700

    ✨ Spring complete

--- src/js/component/render-props/widget/RenderSpring.jsx
+++ src/js/component/render-props/widget/RenderSpring.jsx
@@ -12,6 +12,7 @@ export default class RenderSpring extends Component {
 
         this.state = {
             isReserve: false,
+            config: config.default
         };
     }
 
@@ -28,28 +29,51 @@ export default class RenderSpring extends Component {
         console.log('onFrame');
     }
 
+    handleOnClick(val) {
+        switch (val) {
+            case 1:
+                this.setState({  config: config.default });
+                break;
+            case 2:
+                this.setState({ config: config.gentle });
+                break;
+            case 3:
+                this.setState({ config: { mass: 2.5, tension: 90, friction: 2.5 } });
+                break;
+            default:
+                this.setState({ config: config.default });
+                break;
+        }
+    }
+
     render() {
         return (
-            <div className="container">
-                <div className="x">
-                    <div className="line"></div>
+            <div>
+                <a className="green" onClick={() => this.handleOnClick(1)}>config.default</a>
+                <a className="green" onClick={() => this.handleOnClick(2)}>config.gentle</a>
+                <a className="green" onClick={() => this.handleOnClick(3)}>config.custom</a>
+                <span>{JSON.stringify(this.state.config)}</span>
+                <div className="container">
+                    <div className="x">
+                        <div className="line"></div>
+                    </div>
+                    <div className="y">
+                        <div className="line"></div>
+                    </div>
+                    <Spring
+                        from={{ top: '0px', number: 0 }}
+                        to={{ top: '450px', number: 450 }}
+                        config={this.state.config}
+                        // immediate={true}
+                        reset={true}
+                        // reverse={this.state.isReserve}
+                        onStart={this.handleOnstart.bind(this)}
+                        onRest={this.handleOnRest.bind(this)}
+                        onFrame={this.handleOnFrame.bind(this)}
+                    >
+                        {props => <div className="square" style={props}>{props.number.toFixed(2)}</div>}
+                    </Spring>
                 </div>
-                <div className="y">
-                    <div className="line"></div>
-                </div>
-                <Spring
-                    from={{ top: '0px', number: 0 }}
-                    to={{ top: '900px', number: 900 }}
-                    // config={config.wobbly}
-                    // immediate={true}
-                    reset={true}
-                    reverse={this.state.isReserve}
-                    onStart={this.handleOnstart.bind(this)}
-                    onRest={this.handleOnRest.bind(this)}
-                    onFrame={this.handleOnFrame.bind(this)}
-                >
-                    {props => <div className="square" style={props}>{Math.round(props.number)}</div>}
-                </Spring>
             </div>
         );
     }
