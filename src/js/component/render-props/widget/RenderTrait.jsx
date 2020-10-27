41fa1f281e7b9b3bf0320a0f55c807c2040f6325
Doan Thanh Thang <thangdt@newit.co.jp>
2020-10-27 15:44:41 +0700

    ✨ complete trait

--- src/js/component/render-props/widget/RenderTrait.jsx
+++ src/js/component/render-props/widget/RenderTrait.jsx
@@ -2,12 +2,44 @@
 
 import React, { Component } from 'react';
 
+import { Trail } from 'react-spring/renderprops';
 export default class RenderTrait extends Component {
-    
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            toggle: true,
+            items: ['item1', 'item2', 'item3', 'item4', 'item5']
+        };
+    }
+
+    handleOnClick() {
+        this.setState({ toggle: !this.state.toggle });
+    }
+
     render() {
         return (
-            <div>
-                RenderTrait
+            <div className="container"
+            >
+                <Trail
+                    reverse={this.state.toggle}
+                    initial={null}
+                    items={this.state.items}
+                    from={{ opacity: 0, x: 0 }}
+                    to={{ opacity: this.state.toggle ? 1 : 0.25, x: this.state.toggle ? 0 : 100 }}>
+                    {
+                        (item) => ({ x, opacity }) =>
+                            (<div
+                                className="box"
+                                onClick={this.handleOnClick.bind(this)}
+                                style={{ opacity: opacity, transform: `translate3d(${x}%,0,0)` }}
+                            >
+                                {item}
+                            </div>)
+                    }
+
+                </Trail>
             </div>
         );
     }
