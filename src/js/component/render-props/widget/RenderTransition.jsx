2b0e24a2f496b76f3323f85bb2c0543f0a3b9afb
Đoàn Thanh Thắng <57343206+fukokami@users.noreply.github.com>
2020-11-07 18:23:46 +0700

    ✨ Transition (#5)
    * 💩 need to fix bug delete
    
    * ✨ Transition
    
    * 💄 change style
    
    Co-authored-by: Doan Thanh Thang <thangdt@newit.co.jp>

---
+++ src/js/component/render-props/widget/RenderTransition.jsx
@@ -0,0 +1,58 @@
+'use strict';
+
+
+import React, { Component } from 'react';
+import { Transition } from 'react-spring/renderprops';
+
+export default class RenderTransition extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [1, 2],
+            isEdit: true
+        };
+    }
+
+    addItem() {
+        const { items } = this.state;
+        items.push(items.length + 1);
+        this.setState({ items });
+    }
+
+    deleteItem() {
+        const { items } = this.state;
+        this.setState({ items: items.slice(0, items.length - 1) });
+    }
+
+    render() {
+        return (
+            <div>
+                <a className="green" onClick={() => this.addItem()}>Add</a>
+                <a className="green" onClick={() => this.deleteItem()}>Delete</a>
+                <div className="container">
+                    <Transition
+                        items={this.state.items}
+                        initial={null}
+                        from={{ overflow: 'hidden', height: 0, opacity: 0 }}
+                        enter={{ height: 50, opacity: 1, background: '#28d79f' }}
+                        leave={{ height: 0, opacity: 0, background: '#c23369' }}
+                        update={{ background: '#28b4d7' }}
+                        trail={200}
+                    >
+                        {
+                            (item) => props => {
+                                return (
+                                    <div className="todo" style={props}>
+                                        {item}
+                                    </div>
+                                );
+                            }
+                        }
+                    </Transition>
+                </div>
+            </div>
+        );
+    }
+}
