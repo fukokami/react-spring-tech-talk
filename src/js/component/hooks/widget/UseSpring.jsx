9848a60105631b4548c3e4827b146a828c75c15e
fukokami <doanthanhthang.cntt@gmai.com>
2020-11-11 23:03:10 +0700

    ✨ init useSpring

--- src/js/component/hooks/widget/UseSpring.jsx
+++ src/js/component/hooks/widget/UseSpring.jsx
@@ -1,11 +1,33 @@
 'use strict';
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
 
 export default function UseSpring() {
+
+    const spring = useSpring({
+        from: { top: '0px', number: 0 },
+        to: { top: '450px', number: 450 },
+        reset: true
+    });
+
+    // const [spring, set] = useSpring(() => ({
+    //     from: { top: '0px', number: 0 },
+    // }));
+
+    // useEffect(() => {
+    //     set({ top: '450px', number: 450 });
+    // });
+
     return (
-        <div>
-            Hook Spring
+        <div className="container">
+            <div className="x">
+                <div className="line"></div>
+            </div>
+            <div className="y">
+                <div className="line"></div>
+            </div>
+            <animated.div className="square" style={spring}>{spring.number}</animated.div>
         </div>
     );
 }
